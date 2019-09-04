60b92a6d5ef30224014aebed688eff4a8552c0f9
Shahbaaz <shahbaazchaviwale@gmail.com>
2019-09-04 22:15:41 +0530

    table-list api

---
+++ table-list.ts
@@ -0,0 +1,141 @@
+[{
+    "_id": "01d5ab1a",
+    "subdomain": "wing-1",
+    "domain": "aircraft",
+    "part_id": "019d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "01d5ab1a",
+        "subdomain": "wing-1",
+        "domain": "aircraft",
+        "part_id": "019d5ab1a"
+    }]
+},
+{
+    "_id": "02d5ab1a",
+    "subdomain": "wing-2",
+    "domain": "aircraft",
+    "part_id": "029d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "02d5ab1a",
+        "subdomain": "wing-2",
+        "domain": "aircraft",
+        "part_id": "029d5ab1a"
+    }]
+},
+{
+    "_id": "03d5ab1a",
+    "subdomain": "wing-3",
+    "domain": "aircraft",
+    "part_id": "039d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "03d5ab1a",
+        "subdomain": "wing-3",
+        "domain": "aircraft",
+        "part_id": "039d5ab1a"
+    }]
+},
+{
+    "_id": "04d5ab1a",
+    "subdomain": "wing-4",
+    "domain": "aircraft",
+    "part_id": "049d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "04d5ab1a",
+        "subdomain": "wing-4",
+        "domain": "aircraft",
+        "part_id": "049d5ab1a"
+    }]
+},
+{
+    "_id": "05d5ab1a",
+    "subdomain": "wing-5",
+    "domain": "aircraft",
+    "part_id": "059d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "05d5ab1a",
+        "subdomain": "wing-5",
+        "domain": "aircraft",
+        "part_id": "059d5ab1a"
+    }]
+},
+{
+    "_id": "06d5ab1a",
+    "subdomain": "wing-6",
+    "domain": "aircraft",
+    "part_id": "069d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "06d5ab1a",
+        "subdomain": "wing-6",
+        "domain": "aircraft",
+        "part_id": "069d5ab1a"
+    }]
+},
+{
+    "_id": "07d5ab1a",
+    "subdomain": "wing-7",
+    "domain": "aircraft",
+    "part_id": "079d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "07d5ab1a",
+        "subdomain": "wing-7",
+        "domain": "aircraft",
+        "part_id": "079d5ab1a"
+    }]
+},
+{
+    "_id": "08d5ab1a",
+    "subdomain": "wing-8",
+    "domain": "aircraft",
+    "part_id": "089d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "08d5ab1a",
+        "subdomain": "wing-8",
+        "domain": "aircraft",
+        "part_id": "089d5ab1a"
+    }]
+},
+{
+    "_id": "09d5ab1a",
+    "subdomain": "wing-9",
+    "domain": "aircraft",
+    "part_id": "099d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "09d5ab1a",
+        "subdomain": "wing-9",
+        "domain": "aircraft",
+        "part_id": "099d5ab1a"
+    }]
+},
+{
+    "_id": "10d5ab1a",
+    "subdomain": "wing-10",
+    "domain": "aircraft",
+    "part_id": "109d5ab1a",
+    "info_mimetype": "application/json",
+    "info_dependent": "parent",
+    "nested": [{
+        "_id": "10d5ab1a",
+        "subdomain": "wing-10",
+        "domain": "aircraft",
+        "part_id": "0109d5ab1a"
+    }]
+}
+]                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
